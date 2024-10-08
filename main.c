6287f6b6795b2ae4143a8987b300ab8b3f0aa1be
arthurm170110 <arthur.lima@dcomp.ufs.br>
2024-10-08 00:41:26 -0300

    Atualização das sprites de background e criação do Design do background

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "sprites.c"
+#include "backgrounds.c"
 
 void main(){
+
+    set_bkg_data(0, 16, Fase2_Sprites);
+    set_bkg_tiles(0, 0, 32, 18, Fase2_BackGround);
+
+    SHOW_BKG;
+    DISPLAY_ON;
+
+    while(1){
+        scroll_bkg(1, 0);
+        delay(100);
+    }
 }
