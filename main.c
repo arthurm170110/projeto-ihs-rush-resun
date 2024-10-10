4e20db47df618d6637b90fc0f6935016957d3251
arthurm170110 <arthur.lima@dcomp.ufs.br>
2024-10-09 22:12:34 -0300

    Adicionando música de fundo na fase 1

--- main.c
+++ main.c
@@ -6,6 +6,269 @@
 #include <gb/font.h>
 
 UINT8 x, y;
+UINT8 tocou = 1;
+UINT8 sound_step = 0;
+UINT8 sound_substep = 0;
+UINT8 delay_counter = 0;
+UINT8 i = 0;
+
+
+
+void performantdelay(UINT8 numloops)
+{
+    UINT8 ii;
+    for (ii = 0; ii < numloops; ii++)
+    {
+        wait_vbl_done();
+    }
+}
+
+
+// Função que processa uma nota do som por vez
+void play1_first_part() {
+    if (delay_counter > 0) {
+        delay_counter--;
+        return; // Aguardando o delay entre as notas
+    }
+
+    switch(sound_step) {
+        case 0:
+            NR10_REG = 0x00; // Sol5
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x17;
+            NR14_REG = 0x83;
+            delay_counter = 2;
+            sound_step++;
+            break;
+        
+        case 1:
+            NR10_REG = 0x00; // Muta
+            NR11_REG = 0x00;
+            NR12_REG = 0x00;
+            NR13_REG = 0x00;
+            NR14_REG = 0x80;
+            delay_counter = 1;
+            sound_step++;
+            break;
+        
+        case 2:
+            NR10_REG = 0x00; // Sol5
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x17;
+            NR14_REG = 0x83;
+            delay_counter = 3;
+            sound_step++;
+            break;
+
+        case 3:
+            NR10_REG = 0x00; // Si5
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0xE5;
+            NR14_REG = 0x83;
+            delay_counter = 2;
+            sound_step++;
+            break;
+
+        case 4:
+            NR10_REG = 0x00; // Dó6
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x20;
+            NR14_REG = 0x84;
+            delay_counter = 2;
+            sound_step++;
+            break;
+
+        case 5:
+            NR10_REG = 0x00; // Sol5
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x17;
+            NR14_REG = 0x83;
+            delay_counter = 2;
+            sound_step++;
+            break;
+
+        case 6:
+            NR10_REG = 0x00; // Muta
+            NR11_REG = 0x00;
+            NR12_REG = 0x00;
+            NR13_REG = 0x00;
+            NR14_REG = 0x80;
+            delay_counter = 1;
+            sound_step++;
+            break;
+
+        case 7:
+            NR10_REG = 0x00; // Sol5
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x17;
+            NR14_REG = 0x83;
+            delay_counter = 3;
+            sound_step++;
+            break;
+
+        case 8:
+            NR10_REG = 0x00; // Fá5
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0xC1;
+            NR14_REG = 0x82;
+            delay_counter = 2;
+            sound_step++;
+            break;
+
+        case 9:
+            NR10_REG = 0x00; // Fá#5
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0xEA;
+            NR14_REG = 0x82;
+            delay_counter = 2;
+            sound_step++;
+            break;
+        case 10:
+            NR10_REG = 0x00; // Muta
+            NR11_REG = 0x00;
+            NR12_REG = 0x00;
+            NR13_REG = 0x00;
+            NR14_REG = 0x80;
+            sound_step = 0;
+            if(i == 1){
+                tocou = 0;
+            }
+            i++;
+            break;
+    }
+}
+
+void play1_second_part() {
+    if (delay_counter > 0) {
+        delay_counter--;
+        return; // Aguardando o delay entre as notas
+    }
+
+    switch(sound_step) {
+        case 0:
+            NR10_REG = 0x00;//Lá#6
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x59;
+            NR14_REG = 0x87;
+            delay_counter = 2;
+            sound_step++;
+            break;
+        case 1:
+            NR10_REG = 0x00;//Sol6
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x2E;
+            NR14_REG = 0x86;
+            delay_counter = 2;
+            sound_step++;
+            break;
+        case 2:
+            NR10_REG = 0x00;//Ré6
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0xA1;
+            NR14_REG = 0x84;
+            delay_counter = 10;
+            sound_step++;
+            break;
+        case 3:
+            NR10_REG = 0x00;//Lá#6
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x59;
+            NR14_REG = 0x87;
+            delay_counter = 2;
+            sound_step++;
+            break;
+        case 4:
+            NR10_REG = 0x00;//Sol6
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x2E;
+            NR14_REG = 0x86;
+            delay_counter = 2;
+            sound_step++;
+            break;
+        case 5:
+            NR10_REG = 0x00;//Dó#5
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x2F;
+            NR14_REG = 0x82;
+            delay_counter = 10;
+            sound_step++;
+            break;
+        case 6:
+            NR10_REG = 0x00;//Lá#6
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x59;
+            NR14_REG = 0x87; 
+            delay_counter = 2;
+            sound_step++;
+            break;
+        case 7:
+            NR10_REG = 0x00;//Sol6
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x2E;
+            NR14_REG = 0x86;
+            delay_counter = 2;
+            sound_step++;
+            break;
+        case 8:
+            NR10_REG = 0x00;//Dó6
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x20;
+            NR14_REG = 0x84;
+            delay_counter = 10;
+            sound_step++;
+            break;
+        case 9:
+            NR10_REG = 0x00;//Si5
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0xE5;
+            NR14_REG = 0x83;
+            delay_counter = 2;
+            sound_step++;
+            break;
+        case 10:
+            NR10_REG = 0x00;//Dó6
+            NR11_REG = 0x81;
+            NR12_REG = 0x4B;
+            NR13_REG = 0x20;
+            NR14_REG = 0x84;
+            delay_counter = 2;
+            sound_step++;
+            break;
+        case 11:
+            NR10_REG = 0x00; //Muta
+            NR11_REG = 0x00;
+            NR12_REG = 0x00;
+            NR13_REG = 0x00;
+            NR14_REG = 0x80;
+            delay_counter = 5;
+            sound_step++;
+            break;
+        case 12:
+            sound_step = 0;
+            tocou = 1;
+            i = 0;
+            break;
+        
+    }
+}
 
 
 void limpar_tela(UINT8 tile){
@@ -45,8 +308,17 @@ void fase1(){
     DISPLAY_ON;
 
     while(1){
-        scroll_bkg(2, 0);
-        delay(100);
+        scroll_bkg(3, 0);
+        performantdelay(5);
+
+        
+        if(i < 2){
+            play1_first_part();
+        }
+        if(tocou == 0){
+            play1_second_part();
+        }
+        
 
         if(joypad()){
             break;
@@ -64,8 +336,8 @@ void fase2(){
     DISPLAY_ON;
 
     while(1){
-        scroll_bkg(4, 0);
-        delay(100);
+        scroll_bkg(6, 0);
+        performantdelay(5);
         
 
         if(joypad()){
@@ -79,6 +351,12 @@ void fase2(){
 }
 
 void main(){
+
+    // Inicialização do sistema de som
+    NR52_REG = 0x80; // Liga o som
+    NR50_REG = 0x77; // Volume máximo para os canais esquerdo e direito
+    NR51_REG = 0xFF; // Ativa todos os canais de som
+    
     tela_inicial();
     fase1();
     fase2(); 
---
+++ structs.c
@@ -0,0 +1,17 @@
+#include <gb/gb.h>
+
+typedef struct{
+	UBYTE spritids[4]; // all characters use 4 sprites
+	UINT8 x;
+	UINT8 y;
+	UINT8 width;
+	UINT8 height;
+} MainCharacter;
+
+typedef struct{
+	UBYTE spritids[4]; // all characters use 4 sprites
+	UINT8 x;
+	UINT8 y;
+	UINT8 width;
+	UINT8 height;
+} SecondaryCharacterBoss;
